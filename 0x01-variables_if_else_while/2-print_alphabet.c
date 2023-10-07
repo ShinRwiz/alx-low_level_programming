7e74b61f7d6994d42cb6eb8008813d06ffb70e40
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-06 18:13:31 -0700

    2.4

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,9 @@
 int main(void)
 {
 	int letter;
-  for (letter = 'a'; letter <= 'z'; letter++)
-    putchar(letter);
-  putchar('\n');
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
 	return (0);
 }
