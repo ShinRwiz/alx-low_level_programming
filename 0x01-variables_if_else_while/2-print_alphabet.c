68a99cf2c793fece5760b40e7dc4481cc86dcac7
ShinRwiz <114778876+ShinRwiz@users.noreply.github.com>
2023-10-07 02:11:18 +0100

    2.0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 2-
+ * Return: Always O (Success)
+ */
+
+
+int main(void)
+{
+	int letter;
+  for (letter = 'a'; letter <= 'z'; letter++)
+    putchar(ch);
+  putchar('\n');
+	return (0);
+}
