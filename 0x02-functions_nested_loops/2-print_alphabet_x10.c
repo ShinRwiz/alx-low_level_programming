2897def74387288218311e999777943a37c553df
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-10 21:09:36 -0700

    f

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int cnt = 0;
 	char letter;
 
-	while (count++ <= 9)
+	for (cnt++ <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{
 			_putchar(letter);
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
