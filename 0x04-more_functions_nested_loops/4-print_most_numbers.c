83cd58263f85fcc7a942e4877ca17644a419b19d
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-12 15:51:12 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,12 +5,12 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	int n
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 0; n <= 9; n++)
 	{
-		if !((c = '2' || c = '4'))
-			_putchar(c);
+		if !((n = 2 || n = 4))
+			_putchar(n);
 	}
 	_putchar('\n');
 }
