3ff1010333fbe8823b799c93a3b20d9c0e0c9c7b
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-16 19:23:41 -0700

    d

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include "main.h"
 /**
- *  --
- *  --
+ * puts2 --
+ * @s: --
  * Return: --
  */
+void puts2(char *str)
+{
+	int i, t;
+
+	t = 0;
+
+	while (str[t] != '\0')
+	{
+		t++;
+	}
+
+	for (i = 0; i < t; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
