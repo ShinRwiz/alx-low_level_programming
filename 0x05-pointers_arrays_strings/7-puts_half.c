1daaeab7720c60a8a2fcbd9538c5f2f779489f61
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-16 19:33:20 -0700

    d

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,24 @@
 #include "main.h"
 /**
- *  --
- *  --
+ * puts_half --
+ * @str: --
  * Return: --
  */
+void puts_half(char *str)
+{
+	int t, n, i;
+
+	t = 0;
+
+	while (str[t] != '\0')
+		t++;
+	if (t % 2 == 0)
+	{
+		for (i = t / 2; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	} else if (t % 2)
+		for (n = (t - 1) / 2; n < t - 1; n++)
+		_putchar(str[n + 1]);
+	}
+	_putchar('\n');
+}
