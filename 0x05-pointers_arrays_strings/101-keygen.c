8e67a7000a67158c4bcf83cce43baf2fac802e9e
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-16 20:31:06 -0700

    d

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,33 +1,32 @@
 #include "main.h"
 /**
- *  --
+ * generateRandomChar --
  *  --
  * Return: --
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #define PASSWORD_LENGTH 12
-
-char generateRandomChar() {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const size_t charsetSize = sizeof(charset) - 1;
-    return charset[rand() % charsetSize];
+char generateRandomChar()
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	const size_t charsetSize = sizeof(charset) - 1;
+	return charset[rand() % charsetSize];
 }
 
 int main(void) {
-    srand(time(NULL));
+	srand(time(NULL));
 
-    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+	char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
 
-    for (int i = 0; i < PASSWORD_LENGTH; ++i) {
-        password[i] = generateRandomChar();
-    }
+	for (int i = 0; i < PASSWORD_LENGTH; ++i)
+	{
+		password[i] = generateRandomChar();
+	}
 
-    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
-
-    printf("%s\n", password);
-
-    return 0;
+	password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
+	
+	printf("%s\n", password);
+	return 0;
 }
