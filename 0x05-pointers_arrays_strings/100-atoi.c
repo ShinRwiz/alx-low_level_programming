166fc00668d523bf3f53eb59780972e9f1ad87a1
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-16 21:15:02 -0700

    d

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,22 @@
 #include "main.h"
 /**
- *  --
- *  --
+ * _atoi --
+ * @s: --
  * Return: --
  */
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
 
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * sign);
+}
