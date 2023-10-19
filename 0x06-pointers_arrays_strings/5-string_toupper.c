14d2bb8c42a8766bca2e573d00b2a7c913bd5509
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-19 20:54:19 +0100

    s

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,27 @@
 #include "main.h"
 /**
- * reset_to_98 --
- * @n: --
+ * string_toupper --
+ * @str: --
  * Return: --
  */
+char *string_toupper(char *str)
+{
+	char array1[] = "abcdefghijklmnopqrstuvwxyz";
+	char array2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *ptr = str;
+	int i;
 
+	while (*ptr)
+	{
+		for (int i = 0; array1[i]; i++)
+		{
+			if (*ptr == array1[i])
+			{
+				*ptr = array2[i];
+				break;
+			}
+		}
+			ptr++;
+	}
+	return (str);
+}
