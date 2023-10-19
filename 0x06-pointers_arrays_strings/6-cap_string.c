b350729b796184a708c664ed264da2f3b950beb0
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-19 22:41:44 +0100

    s

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,35 @@
 #include "main.h"
 /**
- * reset_to_98 --
- * @n: --
+ * cap_string --
+ * @s: --
  * Return: --
  */
+char *cap_string(char *s)
+{
+	int i = 0, j = 0;
+	char sr[] = " \t\n,;.!?\"(){}";
 
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			if (i == 0)
+			{
+				s[i] -= 32;
+			}
+			else
+			{
+				for (j = 0; sr[j] != '\0'; j++)
+				{
+					if (s[i - 1] == sr[j])
+					{
+						s[i] -= 32;
+						break;
+					}
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
