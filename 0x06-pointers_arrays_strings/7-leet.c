d72bcad5f26b2baf1c005a47095f1fc787abd251
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-19 23:16:51 +0100

    s

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,13 +10,14 @@ char *leet(char *s)
 	char array1[] = "aAeEoOtTlL";
 	char array2[] = "44370171";
 
-	for (i = 0; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; array1[j]; j++)
 		{
 			if (s[i] == array1[j])
 			{
 				s[i] = array2[j];
+				break;
 			}
 		}
 	}
