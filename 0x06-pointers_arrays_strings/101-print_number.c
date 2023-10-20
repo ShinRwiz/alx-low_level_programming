165f5a66bc339c9314759448894d6d927d822ab7
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-20 02:05:41 +0100

    s

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,4 +4,22 @@
  * @n: --
  * Return: --
  */
+void print_number(int n)
+{
+	unsigned int m;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 > 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
