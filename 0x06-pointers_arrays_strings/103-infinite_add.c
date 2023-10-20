bb56b73416b3fa71bc9192a1a3cf7d40e73a7f11
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-20 02:52:13 +0100

    suffering

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,35 @@
 #include "main.h"
 /**
- * reset_to_98 --
- * @n: --
+ * infinite_add --
+ * @n1: --
+ * @n2: --
+ * @r: --
+ * @size_r: --
  * Return: --
  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int num1, num2, sum, carry = 0;
+	int i = 0, j = 0, k = 0;
 
+	while (n1[i] != '\0' || n2[j] != '\0' || carry)
+	{
+		num1 = (n1[i] != '\0') ? n1[i] - '0' : 0;
+		num2 = (n2[j] != '\0') ? n2[j] - '0' : 0;
+		sum = num1 + num2 + carry;
+		if (k < size_r - 1)
+		{
+			r[k] = (sum % 10) + '0';
+			k++;
+		}
+		else
+		{
+			return (0);
+		}
+		carry = sum / 10;
+		if (n1[i] != '\0') i++;
+		if (n2[j] != '\0') j++;
+	}
+	r[k] = '\0';
+	return (r);
+}
