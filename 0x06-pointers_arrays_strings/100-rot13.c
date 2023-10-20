eba86a0223f027b1df348258f3c497de25281c16
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-20 02:03:09 +0100

    s

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,7 +1,25 @@
 #include "main.h"
 /**
- * reset_to_98 --
- * @n: --
+ * rot13 --
+ * @s: --
  * Return: --
  */
+char *rot13(char *s)
+{
+	int i, j;
+	char a1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char a2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a1[j] == *(s + i))
+			{
+				*(s + i) = a2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
