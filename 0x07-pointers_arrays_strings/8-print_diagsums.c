5aed2335dc5fe9f38b58bd33b44dba5faefa7a74
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-24 03:48:59 +0100

    7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,8 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * _strcat --
- * @dest: --
- * @src: --
+ * print_diagsums --
+ * @a: --
+ * @size: --
  * Return: --
  */
+void print_diagsums(int *a, int size)
+{
+	int s1 = 0, s2 = 0, i;
 
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i][i];
+		s2 += a[(size - 1) - i][i];
+	}
+	printf("%d, %d\n", s1, s2);
+}
