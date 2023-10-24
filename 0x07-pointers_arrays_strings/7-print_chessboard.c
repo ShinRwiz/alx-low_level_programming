d0825faf0230611a2974987bc1c16745fc997b3a
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-24 03:14:36 +0100

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,8 +1,19 @@
 #include "main.h"
 /**
- * _strcat --
- * @dest: --
- * @src: --
+ * print_chessboard --
+ * @a: --
  * Return: --
  */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
 
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
