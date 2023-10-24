5de4fd2bc5da8c999a35688b8338935dc868ff9a
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-24 02:51:35 +0100

    7

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,25 @@
 #include "main.h"
 /**
- * _strcat --
- * @dest: --
- * @src: --
+ * _strpbrk --
+ * @s: --
+ * @accept: --
  * Return: --
  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
 
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}
