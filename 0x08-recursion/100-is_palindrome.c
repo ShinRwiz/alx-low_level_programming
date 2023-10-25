10c86d3b419b87c18da9b0682318fd7d3d7508de
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-25 21:05:51 +0100

    x8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,7 +1,32 @@
 #include "main.h"
 /**
- * print_chessboard --
- * @a: --
+ * comparer --
+ * @s: --
+ * @i: --
+ * @j: --
  * Return: --
  */
+int comparer(char *s, int i, int j)
+{
+	if (i >= j)
+		return (1);
 
+	if (s[i] != s[j])
+		return (0);
+
+	return  (comparer(s, i + 1,  j  - 1));
+}
+/**
+ * is_palindrome --
+ * @s: --
+ * Return: --
+ */
+int is_palindrome(char *s)
+{
+	int size = 0;
+
+	while (s[size] != '\0')
+		size++;
+
+	return (comparer(s, 0, size - 1));
+}
