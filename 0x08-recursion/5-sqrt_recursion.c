7247db0d7515d6458b2452878ca4ccf32a1c8d84
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-25 19:42:38 +0100

    x8

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion --
- * @n: --
- * Return: --
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	return (finder(n, 0));
-}
-/**
  * finder --
  * @n: --
  * @i: --
@@ -29,4 +16,17 @@ int finder(int n, int i)
 		return (i);
 	}
 	return (finder(n, i + 1));
+}
+/**
+ * _sqrt_recursion --
+ * @n: --
+ * Return: --
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (finder(n, 0));
 }
