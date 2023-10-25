01478f67c94294401f74edfe2c996bc5240c511e
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-25 19:23:00 +0100

    x8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * print_chessboard --
+ * @a: --
+ * Return: --
+ */
+
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _sqrt_recursion --
+ * @n: --
+ * Return: --
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (finder(n, 0));
+}
+/**
+ * finder --
+ * @n: --
+ * @i: --
+ * Return: --
+ */
+int finder(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (finder(n, i + 1));
+}
