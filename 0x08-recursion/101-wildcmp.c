4410c9b978fead6a9771bbea9a3d90f17414ff2e
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-25 21:38:55 +0100

    x8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,24 @@
 #include "main.h"
 /**
- * print_chessboard --
- * @a: --
+ * wildcmp --
+ * @s1: --
+ * @s2: --
  * Return: --
  */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
 
+	if (*s2 == '*')
+	{
+		if (*s1 == '\0')
+		return (wildcmp(s1, s2 + 1));
+
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	if (*s1 != *s2)
+		return (0);
+
+	return (wildcmp(s1 + 1, s2 + 1));
+}
