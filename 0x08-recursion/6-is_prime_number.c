ce9b7f36b50369e7cfa08d7e98677190e92ee535
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-25 20:48:10 +0100

    x8

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,28 @@
 #include "main.h"
 /**
+ * finder --
+ * @n: --
+ * @c: --
+ * Return: --
+ */
+int finder(int n, int c)
+{
+	if (c * c > n)
+		return (1);
+	if (n % c == 0)
+		return (0);
+
+	return (finder(n, c + 1));
+}
+/**
  * is_prime_number --
  * @n: --
  * Return: --
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
-	{
 		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+
+	return (finder(n, 2));
 }
