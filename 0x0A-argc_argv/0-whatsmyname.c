25358e44eaae7f4a45e35e4c7f23e051fbe4ae27
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-27 22:47:29 +0100

    c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main --
+ * @argc: --
+ * @argv: --
+ * Return: --
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * main --
+ * @argc: --
+ * @argv: --
+ * Return: --
+ */
+
