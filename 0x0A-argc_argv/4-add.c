e82e7bc4f45d4f63982a4c8805ec1687d62226ce
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-28 00:20:33 +0100

    c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 /**
  * main --
  * @argc: --
  * @argv: --
  * Return: --
  */
+int main(int argc, char *argv[])
+{
+	int a = 0;
+	char *b;
 
+	while (--argc)
+	{
+		for (b = argv[argc]; *c;c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Error\n"));
+			a += atoi(argv[argc]);
+	}
+	printf("%d\n", a);
+	return (0);
+}
