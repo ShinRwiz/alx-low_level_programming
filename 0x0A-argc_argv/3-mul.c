cc94c9a469a17a04ef658d3715944a46aea8d8fb
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-10-27 23:43:17 +0100

    c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,4 +5,19 @@
  * @argv: --
  * Return: --
  */
+int main(int argc, char *argv[])
+{
+	int m;
 
+	if (argc == 3)
+	{
+		m = (atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", m);
+	}
+	else
+	{
+		printf("Something ain't Right\n");
+		return (1);
+	}
+	return (0);
+}
