7cdea59779779dd3c75d5fbda37fc3985d859117
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-01 05:43:47 +0100

    0x0B

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,5 +29,46 @@ int wc(char *s)
  */
 char **strtow(char *str)
 {
+	int i = 0, j, k, l, m = 0, n = 0;
+	char **s;
 
+	if (str == 0 || *str == '\0')
+		return (NULL);
+
+	n = wc(str);
+	if (n == 1)
+		return(NULL);
+
+	s = (char **)malloc(n * sizeof(char *));
+	if (s == NULL)
+		return (NULL);
+
+	s[n -  1] = NULL;
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		{
+			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+				;
+			j++;
+			s[m] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (s[m] == NULL)
+			{
+				for (k = 0; k < m; k++)
+					free(s[k]);
+				free(s[n - 1]);
+				free(s);
+				return (NULL);
+			}
+			for (l = 0; l < j; l++)
+				s[m][l] = str[i + l];
+			s[m][l] = '\0';
+			m++;
+			i += j;
+		}
+		else
+			i++;
+	}
+	return (s);
 }
