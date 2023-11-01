0e3e264e25a80aa53b3c8b306e9f9cd1f9f4ec60
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-01 05:06:13 +0100

    0x0B

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,9 +1,33 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main --
- * @argc: --
- * @argv: --
+ * wc --
+ * @s: --
  * Return: --
  */
+int wc(char *s)
+{
+	int w = 0;
 
+	while (*s != '\0')
+	{
+		if (*s == ' ')
+			s++;
+		else
+		{
+			while (*s != ' ' && *s != '\0')
+				s++;
+			w++;
+		}
+	}
+	return (w);
+}
+/**
+ * strtow --
+ * @str: --
+ * Return: --
+ */
+char **strtow(char *str)
+{
+
+}
