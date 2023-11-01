f759821b45c280aab8160729919eeea30c881a01
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-01 03:41:56 +0100

    0x0B

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * main --
- * @argc: --
- * @argv: --
+ * _strdup --
+ * @str: --
  * Return: --
  */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, size = 0;
 
+	if (str == NULL)
+		return (NULL);
+
+	while (str[size] != '\0')
+		size++;
+
+	s = (char *)malloc((sizeof(char) * size) + 1);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = str[i];
+	s[size] = '\0';
+
+	return (s);
+}
