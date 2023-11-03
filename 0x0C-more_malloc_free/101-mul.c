384e92f4c6c980b9a24b10acc7b36bb21eebf63b
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-03 04:25:10 +0100

    0x0C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _puts --
+ * @str: --
+ * Return: --
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+/**
  * main --
  * @argc: --
  * @argv: --
@@ -8,5 +24,11 @@
  */
 int main(int argc, char *argv[])
 {
+	
+	if (argc != 3)
+	{
+		_puts("Error")
+		exit(98);
+	}
 	return (0);
 }
