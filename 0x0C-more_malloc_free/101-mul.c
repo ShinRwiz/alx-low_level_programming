fb4dec95f79e561e84f093d787ec58628c3ce61b
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-03 05:24:40 +0100

    0x0C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * print_number --
+ * @n: --
+ * Return: --
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
+}
+/**
+ * _atoi --
+ * @s: --
+ * Return: --
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0;
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * sign);
+}
+/**
  * _puts --
  * @str: --
  * Return: --
@@ -24,11 +63,15 @@ void _puts(char *str)
  */
 int main(int argc, char *argv[])
 {
-	
+	long int a;
+
 	if (argc != 3)
 	{
 		_puts("Error");
 		exit(98);
 	}
+	a = (_atoi(argv[1]) * _atoi(argv[2]));
+	print_number(a);
+	_putchar('\n');
 	return (0);
 }
