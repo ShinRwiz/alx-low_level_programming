8aa2219e83e6e6492a4469f222d6a09164716558
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-06 19:59:19 +0100

    0x0C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog --
- *
  * @name: --
- *
  * @age: --
- *
  * @owner: --
  */
-
 struct dog
 {
 	char *name;
@@ -26,5 +21,4 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
-
 #endif
