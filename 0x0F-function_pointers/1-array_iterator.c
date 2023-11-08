93303956da8e5c63212adab1794ca63b5eb69550
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-09 00:19:50 +0100

    0x0C

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator --
+ * @array: --
+ * @size: --
+ * @action: --
+ * Return: --
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && size > 0 && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+}
