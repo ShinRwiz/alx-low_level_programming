a4a80ace4c3177be3c89576e1c02710dcd220648
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-09 00:32:43 +0100

    0x0C

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,16 +11,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array && cmp)
+	if (array && size && cmp)
 	{
 		while (i < size)
 		{
 			if (cmp(array[i]))
+			{
 				return (i);
-
+			}
 			i++
 		}
 	}
-
 	return (-1);
 }
