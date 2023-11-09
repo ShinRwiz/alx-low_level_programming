47b4add04f0f2600bb43d4149c4aaeedce5ed05e
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-09 01:25:53 +0100

    0x0C

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,12 +10,15 @@
 int main(int argc, char **argv)
 {
 	int (*opf)(int, int);
+	int n1, n2;
 
 	if (argc != 4)
 	{
 		printf("Error\n"), exit(98);
 	}
 
+	n1 = atoi(argv[1])
+	n2 = atoi(argv[3])
 	opf = get_op_func(argv[2]);
 
 	if (!opf)
@@ -23,6 +26,11 @@ int main(int argc, char **argv)
 		printf("Error\n"), exit(99);
 	}
 
-	printf("%d\n", opf(atoi(argv[1]), atoi(argv[3])));
+	if ((*opf == '/' || *opf == '%') && n2 == 0)
+	{
+		printf("Error\n"), exit(100);
+	}
+
+	printf("%d\n", get_op_func(opf)(n1, n2));
 	return (0);
 }
