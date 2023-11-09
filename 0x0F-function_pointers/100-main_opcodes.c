94878736bfce238593973df642ef8584cd8d06db
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-09 01:43:55 +0100

    0x0C

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main --
+ * @argc: --
+ * @argv: --
+ * Return: --
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc != 2)
+	{
+		printf("Error\n"), exit(1);
+	}
+
+	a = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n"), exit(2);
+	}
+
+	return (0);
+}
