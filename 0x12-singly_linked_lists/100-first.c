7882c53d603a1d0e8e009ca479bc5c85b8a29b66
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-17 03:47:00 +0100

    0x12

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,11 @@
 #include "lists.h"
-/**
- * add_node --
- * @head: --
- * @str: --
- * Return: --
- */
+void prinfirst(void) __attribute__ ((constructor));
 
+/**
+ * prinfirst - prints a sentence before the main
+ */
+void prinfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
