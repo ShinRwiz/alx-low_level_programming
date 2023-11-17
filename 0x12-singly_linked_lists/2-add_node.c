f14cf10e0aa97332f065c06d9a1dabd170cbbd38
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-17 03:09:56 +0100

    0x12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -4,4 +4,21 @@
  * @h: --
  * Return: --
 */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n = = malloc(sizeof(list_t));
+	unsigned int c = 0;
 
+	while (str[c] != '\0')
+		c++;
+
+	if (n == NULL)
+		return (NULL);
+
+	n->str = strdup(str);
+	n->c = c;
+	n->next = (*head);
+	(*head) = n;
+
+	return (*head);
+}
