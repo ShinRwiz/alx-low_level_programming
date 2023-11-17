7fbb764ff263206d2351c5377d20b5c6a43db138
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-17 02:48:15 +0100

    0x12

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,17 @@
 #include "lists.h"
 /**
- * print_list --
+ * list_len --
  * @h: --
  * Return: --
 */
+size_t list_len(const list_t *h)
+{
+	size_t cn = 0;
 
+	while (h)
+	{
+		cn++;
+		h = h->next;
+	}
+	return (cn);
+}
