188e0c9c2d16a4bd942e5decc4a0d4bd33d4fa76
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-22 02:58:27 +0100

    0x13.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *pr, *next;
 
-	if (!head|| !*head)
+	if (!head || !*head)
 		return (-1);
 
 	if (!index)
