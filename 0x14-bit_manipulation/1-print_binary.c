571127457359c55029051958d4966a5ca0e9deea
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-24 02:43:32 +0100

    0x14.

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,5 +6,18 @@
 */
 void print_binary(unsigned long int n)
 {
+	int i = sizeof(unsigned long int) * 8 - 1, p = 0;
 
+	if (n == 0)
+		_putchar('0');
+	for (; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			_putchar('1');
+			p = 1;
+		}
+		else if (p)
+			_putchar('0');
+	}
 }
