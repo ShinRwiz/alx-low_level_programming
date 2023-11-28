a67450e33f3e89833204daf8ccbd11b0a7236da4
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-28 04:39:23 +0100

    0x15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,14 @@
 #include "main.h"
 /**
+ * error_98 --
+ * @s: The error message to print
+ */
+void error_98(const char *s)
+{
+	dprintf(STDERR_FILENO, "%s\n", s);
+	exit(98);
+}
+/**
  * main --
  * @argc: --
  * @argv: --
@@ -7,5 +16,15 @@
  */
 int main(int argc, char **argv)
 {
+	int file;
 
+	if (argc != 2)
+		error_98("Usage: elf_header elf_filename");
+
+	file = open(argv[1], O_RDONLY);
+	if (file == -1)
+		error_98("Error: Can't open file");
+
+	close(file);
+	return (0);
 }
