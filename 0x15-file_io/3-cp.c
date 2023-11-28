b65241ec3ac199cc999a16b8060cd3cd5cc81dfe
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-28 04:01:27 +0100

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -38,13 +38,9 @@ int main(int argc, char **argv)
 	}
 
 	if (close(from) == -1)
-	{
 		dprintf(2, "Error: Can't close fd %d\n", from), exit(100);
-	}
 
 	if (close(to) == -1)
-	{
 		dprintf(2, "Error: Can't close fd %d\n", to), exit(100);
-	}
 	return (0);
 }
