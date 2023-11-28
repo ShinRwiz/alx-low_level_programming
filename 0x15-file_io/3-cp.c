ebbee136a4faeba066b0eaec379aaea46800ac1e
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-28 03:57:36 +0100

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ if (from == -1)
 
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), close(from), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((r = read(from, buffer, 1024)) > 0)
 	{
