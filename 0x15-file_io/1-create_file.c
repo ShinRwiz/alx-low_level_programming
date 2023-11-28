1908053caddca6c1ce6e84219e78bc3a8a2edc74
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-28 02:25:45 +0100

    0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, w, len;
+	int file, w, len = 0;
 
 	if (!filename)
 		return (-1);
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (!file)
 		return (-1);
 
-	if (!text_content)
+	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
 			len++;
