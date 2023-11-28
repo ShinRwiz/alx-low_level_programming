33320c5beb8937b802269e744bb72d371e375a59
Ahmed <114778876+ShinRwiz@users.noreply.github.com>
2023-11-28 04:05:49 +0100

    0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]), exit(97);
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
